465b60ac4dac6b463406757e0a02e54407d0b1d1
Tom <tchulick48@gmail.com>
2020-03-02 20:52:52 -0600

    added contacts

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,11 @@
 class Api::ContactsController < ApplicationController
+  def second_contact
+    @contact = Contact.second
+    render "contacts.json.jb"
+  end
+
+  def all_contacts
+    @all_contacts = Contact.all
+    render "contact_list.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/contacts_page" => "contacts#second_contact"
+    get "/all_contacts" => "contacts#all_contacts"
+  end
 end
