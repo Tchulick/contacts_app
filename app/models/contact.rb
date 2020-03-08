1dbe670315dc4a81f3801cf09f20d0766c0bc3a9
Tom <tchulick48@gmail.com>
2020-03-08 11:37:51 -0500

    Created new model methods for full name, and time when last updated

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("Updated on %m/%d/%Y at %I:%M%p")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+    return full_name
+  end
 end
